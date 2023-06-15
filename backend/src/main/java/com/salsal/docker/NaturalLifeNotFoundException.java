5dd869f0e4965014e59779ed0772bbc7390cca4a
salsabeel-alsahory <salsabeel2018as@gmail.com>
2023-06-15 17:02:13 +0300

    final version of project

---
+++ backend/src/main/java/com/salsal/docker/NaturalLifeNotFoundException.java
@@ -0,0 +1,7 @@
+package com.salsal.docker;
+public class NaturalLifeNotFoundException extends RuntimeException {
+
+    public NaturalLifeNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import NaturalLifeCrud from './component/naturalLifeCrud'
+function App() {
+  return (
+    <div className="App">
+     <NaturalLifeCrud/>
+    </div>
+  );
+}
+
+export default App;
